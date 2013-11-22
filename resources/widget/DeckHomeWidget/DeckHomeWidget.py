eba57b1d00b00f32f04c98017f620f2228d81749
sernst <swernst@gmail.com>
2013-11-22 01:06:13 -0800

    Updated Windows executable.

--- resources/widget/DeckHomeWidget/DeckHomeWidget.py
+++ resources/widget/DeckHomeWidget/DeckHomeWidget.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file '/Users/scott/Python/CompilerDeck/resources/widget/DeckHomeWidget/DeckHomeWidget.ui'
+# Form implementation generated from reading ui file 'F:\Python\CompilerDeck\resources\widget\DeckHomeWidget\DeckHomeWidget.ui'
 #
-# Created: Thu Nov 14 21:11:36 2013
-#      by: pyside-uic 0.2.13 running on PySide 1.1.1
+# Created: Fri Nov 22 01:03:15 2013
+#      by: pyside-uic 0.2.14 running on PySide 1.1.2
 #
 # WARNING! All changes made in this file will be lost!
 
