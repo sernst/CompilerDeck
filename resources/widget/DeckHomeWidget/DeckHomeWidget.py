0f6979230c5bfd89f33d9cf14a781676cb40d6da
Scott <swernst@gmail.com>
2014-05-28 13:39:19 -0500

    Updated windows build.

--- resources/widget/DeckHomeWidget/DeckHomeWidget.py
+++ resources/widget/DeckHomeWidget/DeckHomeWidget.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file '/Users/scott/Python/CompilerDeck/resources/widget/DeckHomeWidget/DeckHomeWidget.ui'
+# Form implementation generated from reading ui file 'C:\Users\scott\Documents\GitHub\CompilerDeck\resources\widget\DeckHomeWidget\DeckHomeWidget.ui'
 #
-# Created: Wed May 28 13:23:35 2014
+# Created: Wed May 28 13:38:07 2014
 #      by: pyside-uic 0.2.15 running on PySide 1.2.1
 #
 # WARNING! All changes made in this file will be lost!
