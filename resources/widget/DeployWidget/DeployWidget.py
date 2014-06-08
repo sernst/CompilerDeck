9afc53aac89dbdda120a561f4daca4c768a7ae85
sernst <swernst@gmail.com>
2014-06-07 22:09:11 -0500

    Updated OSX build.

--- resources/widget/DeployWidget/DeployWidget.py
+++ resources/widget/DeployWidget/DeployWidget.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'C:\Users\scott\Documents\GitHub\CompilerDeck\resources\widget\DeployWidget\DeployWidget.ui'
+# Form implementation generated from reading ui file '/Users/scott/Python/CompilerDeck/resources/widget/DeployWidget/DeployWidget.ui'
 #
-# Created: Wed May 28 16:45:26 2014
+# Created: Sat Jun  7 22:07:22 2014
 #      by: pyside-uic 0.2.15 running on PySide 1.2.1
 #
 # WARNING! All changes made in this file will be lost!
--- resources/widget/LocalSettingsWidget/LocalSettingsWidget.py
+++ resources/widget/LocalSettingsWidget/LocalSettingsWidget.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'C:\Users\scott\Documents\GitHub\CompilerDeck\resources\widget\LocalSettingsWidget\LocalSettingsWidget.ui'
+# Form implementation generated from reading ui file '/Users/scott/Python/CompilerDeck/resources/widget/LocalSettingsWidget/LocalSettingsWidget.ui'
 #
-# Created: Wed May 28 16:45:26 2014
+# Created: Sat Jun  7 22:07:23 2014
 #      by: pyside-uic 0.2.15 running on PySide 1.2.1
 #
 # WARNING! All changes made in this file will be lost!
